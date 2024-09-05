fcc9c30dfc0245ab33c38d00fbd9b4c03bd67c01
ANKITA7722 <ANKITA7722>
2024-09-05 18:45:24 +0100

    formvalidation

---
+++ insert.js
@@ -0,0 +1,11 @@
+document.getElementById("btn1").addEventListener('click',insert);
+
+async function insert(){
+    try{
+        let myemployee = document.getElementById("eno").value;
+        let myname = document.getElementById("nm").value;
+        let mycity = document.getElementById("city").value;
+        let mysalary = document.getElementById("salary").value;
+    }
+    
+}
